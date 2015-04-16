d9fc70e55326625efd1aa6ec650bd9b082ee3253
Jeon <rainsy02@gmail.com>
2015-04-17 04:52:13 +0900

    modifiy app.js change jwt local, and finish goal, saving_list

---
+++ connectors/saving.js
@@ -0,0 +1,21 @@
+var conn = require('./db.js');
+
+//READ GET
+exports.read = function(req, res){
+	console.log("saving list call");
+	conn.getConnection(function(err,connection){
+	if(err){
+		console.error('MySQl connection err');
+		throw err;
+	}
+	var Query = conn.query('select * from saving_list where fk_kids = ? ',req.user.fk_kids, function(err, rows){
+		if(err){
+			connection.release();
+		}
+		console.log(rows);
+		res.status(200).send(rows);
+		connection.release();
+	});
+	});
+}
+
