fe5560c9c603532be7f0a95456d4d61094c8f692
YeoKyuLi <kyuli.yeo@gmail.com>
2015-05-08 17:52:52 +0900

    db

--- connectors/db.js
+++ connectors/db.js
@@ -6,9 +6,6 @@ var db = express.Router();
 db = mysql.createPool({
 	host : 'localhost',
 	port : 3306,
-	user : 'kyuli.yeo',
-	password : '1234',
-	database : 'coinpet',
 	connectionLimit : 20,
 	multipleStatements : true,
 	waitForConnections:false
--- routes/db-jeon.js
+++ routes/db-jeon.js
@@ -6,9 +6,6 @@ var db = express.Router();
 db = mysql.createPool({
 	host : 'localhost',
 	port : 3306,
-	user : 'coinpet',
-	password : 'dbaudghks',
-	database : 'coinpet',
 	connectionLimit : 20,
 	multipleStatements : true,
 	waitForConnections:false
