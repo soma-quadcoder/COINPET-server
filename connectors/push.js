3f8ec13a9ad65f70bc6b32a3b210219bf4494e16
YeoKyuLi <kyuli.yeo@gmail.com>
2015-05-19 02:32:54 +0900

    waterfall test

--- connectors/push.js
+++ connectors/push.js
@@ -35,7 +35,7 @@ exports.pushInfo = function(req, res){
                     //console.log(rows);
                     //res.status(200).send(rows);
                 });
-                res.status(200).send(rows);
+                res.status(200).json(rows);
                 callback(null);
             },
             function(callback) {
@@ -47,7 +47,7 @@ exports.pushInfo = function(req, res){
                     //console.log(rows);
                     //res.status(200).send(rows);
                     });
-                    res.status(200).send(rows);
+                    res.status(200).json(rows);
                     callback(null);
             }
             ],function(err, result) {
