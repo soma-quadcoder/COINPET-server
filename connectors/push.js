e456122e15c88e85cadde12740be3cb02a6edf4a
YeoKyuLi <kyuli.yeo@gmail.com>
2015-05-22 22:11:55 +0900

    water fall test

--- connectors/push.js
+++ connectors/push.js
@@ -19,7 +19,7 @@ exports.pushQeustAndQuizInfoToApp = function(req, res){
         var pk_std_quiz;
         var pk_parents_quest;
         var pk_std_que;
-        var results =[];
+        var results =[{}];
 
         results.systemQuest = ['a : 1' , ' ssss ', ' sssssss'];
         results.systemQuiz = ['a : 2' , ' ssss ', ' sssssss'];
