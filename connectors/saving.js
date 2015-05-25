5db0dd12b2563dc6595cb0137e6e0bf587a3b3cd
jeon <rainsy02@gmail.com>
2015-05-25 22:05:29 +0900

    modify connection.release() duplicated and add function for parents

--- connectors/saving.js
+++ connectors/saving.js
@@ -7,9 +7,45 @@ exports.read = function(req, res){
 	if(err){
 		console.error('MySQl connection err');
 	}
+
+	if(req.user.fk_parents)
+	{
+		var Query = conn.query('select s.* from saving_list s, parents_has_kids p where s.fk_kids = p.fk_kids AND p.fk_parents = ?', req.user.fk_parents, function(err, rows)
+		{
+			if(err) {
+				connection.release();
+				console.log(err);
+				res.status(500).send();
+				return;
+			}
+
+			var resData = {};
+			for(var i in rows)
+			{
+				var data = rows[i];
+				var fk_kids = data.fk_kids;
+				
+				delete data.fk_kids;
+
+				if(!resData[fk_kids])
+				{
+					resData[fk_kids] = [];
+				}
+				resData[fk_kids].push(data);
+			}
+
+			console.log(resData);
+			res.json(resData);
+			connection.release();
+		});
+		return;
+	}
+
 	var Query = conn.query('select * from saving_list where fk_kids = ? ',req.user.fk_kids, function(err, rows){
 		if(err){
 			connection.release();
+			res.status(500).send();
+			return;
 		}
 		console.log(rows);
 		res.json(rows);
@@ -29,6 +65,7 @@ exports.readParents = function(req, res){
 					"p.fk_kids = " + req.params.fk_kids + " AND " +
 					"s.fk_kids = " + req.params.fk_kids;
 	var Query = conn.query("SELECT s.* FROM saving_list s, parents_has_kids p  WHERE "+condition, function(err, rows){
+		console.log(Query.sql);
 		if(err){
 			connection.release();
 			console.log(err);
