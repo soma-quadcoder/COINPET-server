acca5d3b7c4c39a9c37d44f4a594ba2f3f6b4b5c
jeon <rainsy02@gmail.com>
2015-06-13 22:35:58 +0900

    add GET /admin/user

--- routes/admin/user.js
+++ routes/admin/user.js
@@ -17,23 +17,54 @@ conn.getConnection(function(err){
 
 // nothing
 router.get('/', ejwt({secret: secretKey}), function(req, res) {
+    console.log('GET /admin/user is called');
     if(req.user.fk_admin == 0) {
 	console.log('Access denined');
 	res.status(403).json({'error':'Access denined'});
 	return;
     }
 
-    conn.query("SELECT COUNT(*) FROM kids", function(err, result) {
+    conn.query("SELECT COUNT(*) FROM kids;SELECT COUNT(*) FROM parents;"
+		+"SELECT COUNT(*) FROM kids WHERE createTime BETWEEN CURDATE()-14 AND CURDATE()-7;"
+		+"SELECT COUNT(*) FROM parents WHERE createTime BETWEEN CURDATE()-14 AND CURDATE()-7;"
+		+"SELECT COUNT(*) FROM kids WHERE createTime>CURDATE()-7;"
+		+"SELECT COUNT(*) FROM parents WHERE createTime>CURDATE()-7;"
+		+"SELECT COUNT(*) FROM kids WHERE createTime BETWEEN SUBDATE(CURDATE(), INTERVAL 2 MONTH) AND SUBDATE(CURDATE(), INTERVAL 1 MONTH);"
+		+"SELECT COUNT(*) FROM parents WHERE createTime BETWEEN SUBDATE(CURDATE(), INTERVAL 2 MONTH) AND SUBDATE(CURDATE(), INTERVAL 1 MONTH);"
+		+"SELECT COUNT(*) FROM kids WHERE createTime > SUBDATE(CURDATE(), INTERVAL 1 MONTH);"
+		+"SELECT COUNT(*) FROM parents WHERE createTime > SUBDATE(CURDATE(), INTERVAL 1 MONTH);"
+		, function(err, rows) {
 	    if(err){
     		console.log("Error : Cannot execute query");
 	        console.log(err);
 	        console.log(this.sql);
 	        res.status(500).json({"error":"Fail_query"});
 	    }
-	    else {
-		console.log(result);
-		res.status(200).send();
-	    }
+	        var result = {};
+		result.total = {};
+		result.week = {};
+		result.week.prev = {};
+		result.week.cur = {};
+		result.month = {};
+		result.month.prev = {};
+		result.month.cur = {};
+		
+		var i = 0;
+		result.total.kids = rows[i++][0]['COUNT(*)'];
+		result.total.parents = rows[i++][0]['COUNT(*)'];
+		
+		result.week.prev.kids = rows[i++][0]['COUNT(*)'];
+		result.week.prev.parrent = rows[i++][0]['COUNT(*)'];
+		result.week.cur.kids = rows[i++][0]['COUNT(*)'];
+		result.week.cur.parents = rows[i++][0]['COUNT(*)'];
+
+		result.month.prev.kids = rows[i++][0]['COUNT(*)'];
+		result.month.prev.parrent = rows[i++][0]['COUNT(*)'];
+		result.month.cur.kids = rows[i++][0]['COUNT(*)'];
+		result.month.cur.parents = rows[i++][0]['COUNT(*)'];
+
+		res.status(200).json(result);
+	    
     });
 });
 
